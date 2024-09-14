855090fdd51dafd06b2f3f08cc3b9adfedb576df
juanl <juanlucas.paiva@hotmail.com>
2024-09-14 20:21:55 -0300

    Ajustes para aceitar pilhas genéricas

--- AFDePDA/Controllers/HomeController.cs
+++ AFDePDA/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 public class HomeController : Controller
@@ -11,18 +13,43 @@ public class HomeController : Controller
     [HttpPost]
     public ActionResult ExecutarAutomato(string tipoAutomato, string entrada)
     {
-        ViewBag.TipoAutomatoSelecionado = tipoAutomato; // Armazenando o valor selecionado na ViewBag
+        ViewBag.TipoAutomatoSelecionado = tipoAutomato;
+
+        // Verificar se a entrada contém apenas caracteres permitidos
+        if (!entrada.All(c => c == 'a' || c == 'b' || c == '0' || c == '1'))
+        {
+            ViewBag.Erro = "Entrada inválida. As entradas permitidas são: 'a', 'b', '0', '1'.";
+            return View("Index");
+        }
+
+        // Converter a entrada para o formato adequado
+        IEnumerable<char> entradaCaracteres = entrada.ToCharArray();
+        IEnumerable<int> entradaInteiros = entrada.Select(c => c == 'a' ? 0 : c == 'b' ? 1 : int.Parse(c.ToString()));
 
         if (tipoAutomato == "AFD")
         {
-            // Chamada para executar o AFD com a entrada fornecida
-            bool resultado = ExecutarAFD(entrada);
+            bool resultado;
+            if (entrada.All(c => c == 'a' || c == 'b')) // Verifica se a entrada é string
+            {
+                resultado = ExecutarAFD(entradaCaracteres);
+            }
+            else // Se não é string, deve ser inteiro
+            {
+                resultado = ExecutarAFD(entradaInteiros);
+            }
             ViewBag.Resultado = resultado ? "Aceito pelo AFD" : "Rejeitado pelo AFD";
         }
         else if (tipoAutomato == "PDA")
         {
-            // Chamada para executar o PDA com a entrada fornecida
-            bool resultado = ExecutarPDA(entrada);
+            bool resultado;
+            if (entrada.All(c => c == 'a' || c == 'b')) // Verifica se a entrada é string
+            {
+                resultado = ExecutarPDA(entradaCaracteres);
+            }
+            else // Se não é string, deve ser inteiro
+            {
+                resultado = ExecutarPDA(entradaInteiros);
+            }
             ViewBag.Resultado = resultado ? "Aceito pelo PDA" : "Rejeitado pelo PDA";
         }
         else
@@ -33,48 +60,51 @@ public class HomeController : Controller
         return View("Index");
     }
 
-
-    // Método para executar o AFD aab*aa ou a^n^b^m, sendo n um número par
-    private bool ExecutarAFD(string entrada)
+    // Método para executar o AFD aab*aa ou a^n^b^m, sendo n um número par. Para o computador 0 significa a e 1 b
+    private bool ExecutarAFD<T>(IEnumerable<T> entrada)
     {
-        int contadorA = 0;
+        Stack<T> pilha = new Stack<T>();
 
-        foreach (char c in entrada)
+        foreach (T item in entrada)
         {
-            if (c == 'a')
-                contadorA++;
+            // Se o item for 'a' ou 0, empilhamos
+            if (item.Equals((T)Convert.ChangeType('a', typeof(T))) || item.Equals((T)Convert.ChangeType(0, typeof(T))))
+            {
+                pilha.Push(item);
+            }
         }
-        // Aceita se o número de 'a's for par
-        return contadorA % 2 == 0;
+
+        // Aceita se o número de 'a's ou 0's for par
+        return pilha.Count % 2 == 0;
     }
 
-    // Método para executar o PDA a*b* ou a^n^b^n, sendo n >= 0
-    private bool ExecutarPDA(string entrada)
+    // Método para executar o PDA a*b* ou a^n^b^n, sendo n >= 0. Para o computador 0 significa a e 1 b
+    private bool ExecutarPDA<T>(IEnumerable<T> entrada)
     {
-        int contadorA = 0;
+        Stack<T> pilha = new Stack<T>();
 
-        foreach (char c in entrada)
+        foreach (T item in entrada)
         {
-            if (c == 'a')
+            // Se o item for 'a' ou 0, empilha
+            if (item.Equals((T)Convert.ChangeType('a', typeof(T))) || item.Equals((T)Convert.ChangeType(0, typeof(T))))
             {
-                contadorA++; // Incrementa o contador para cada 'a' lido
+                pilha.Push(item);
             }
-            else if (c == 'b')
+            // Se o item for 'b' ou 1, desempilha
+            else if (item.Equals((T)Convert.ChangeType('b', typeof(T))) || item.Equals((T)Convert.ChangeType(1, typeof(T))))
             {
-                // Se houver 'a's abertos, decrementa o contador
-                if (contadorA > 0)
+                if (pilha.Count > 0)
                 {
-                    contadorA--;
+                    pilha.Pop();
                 }
                 else
                 {
-                    return false; // Se não, a string é rejeitada
+                    return false; // Rejeita se tentar desempilhar sem itens na pilha
                 }
             }
         }
 
-        // A string é aceita se não houver 'a's abertos no final
-        return contadorA == 0;
+        // Aceita se a pilha estiver vazia no final
+        return pilha.Count == 0;
     }
-
 }
